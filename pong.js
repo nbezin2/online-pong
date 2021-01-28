7890e1cf9fdf4c2ecdef93eb5157e85a7767ca1e
nbezin2 <47515136+nbezin2@users.noreply.github.com>
2021-01-27 19:45:07 -0600

    adding socket stuff

--- pong.js
+++ pong.js
@@ -5,4 +5,12 @@ var server = app.listen(process.env.PORT || 3000);
 
 app.use(express.static('Public'));
 
+var socket = require('socket.io');
 
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+  console.log(socket.id);
+}
